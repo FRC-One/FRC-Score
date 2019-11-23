f45b193c83b17850774388639386443f3a340c1e
Alexander Beaver <alex@beaver.dev>
2019-11-22 21:40:38 -0800

    Add track page

--- app.go
+++ app.go
@@ -24,10 +24,28 @@ func returnGlobalHTML(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func returnTrackPage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("html/track.html", "html/pageStart.html", "html/pageEnd.html")
+
+	if err == nil{
+		w.WriteHeader(200)
+		err = t.ExecuteTemplate(w, "track.html", nil)
+		if err != nil{
+			panic(err)
+		}
+
+
+	} else{
+		fmt.Println(err)
+	}
+
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", returnGlobalHTML)
+	r.HandleFunc("/track", returnTrackPage)
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 
